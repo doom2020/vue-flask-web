210c22921c22a657a906c17c161135f6d702f458
yuanzhijian <v-yuanzhijian@xiaomi.com>
2020-12-23 21:10:56 +0800

    找回密码页面基本实现(编译有bug)

--- src/views/forget/Forget.vue
+++ src/views/forget/Forget.vue
@@ -11,20 +11,21 @@
     <form id="findPwdForm" v-if="state.isFindPwd">
       <div class="form-group row">
         <label for="accountForFind" class="col-sm-2 col-form-label">用户名</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control is-valid" id="accountForFind" v-model="state.findForm.account">
+        <div class="col-sm-8">
+          <input type="text" :class="state.findForm.accountClass" id="accountForFind" v-model="state.findForm.account" @blur="handlerAccountForFind">
         </div>
       </div>
       <div class="form-group row">
         <label for="phoneForFind" class="col-sm-2 col-form-label">手机</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control is-valid" id="phoneForFind" v-model="state.findForm.phone">
+        <div class="col-sm-8">
+          <input type="text" :class="state.findForm.phoneClass" id="phoneForFind" v-model="state.findForm.phone" @blur="handlerPhoneForFind">
         </div>
+        <button class="col-sm-2 btn btn-primary" style="font-size: 6px;" @click="toGetCodeForFind">获取验证码</button>
       </div>
       <div class="form-group row">
         <label for="codeForFind" class="col-sm-2 col-form-label">验证码</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control is-valid" id="codeForFind" v-model="state.findForm.code">
+        <div class="col-sm-8">
+          <input type="text" :class="state.findForm.codeClass" id="codeForFind" v-model="state.findForm.code" @blur="handlerCodeForFind">
         </div>
       </div>
       <div class="form-group row" style="margin-top: 50px;">
@@ -36,32 +37,33 @@
     <form id="resetPwdForm" v-if="!state.isFindPwd">
       <div class="form-group row">
         <label for="accountForReset" class="col-sm-2 col-form-label">用户名</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control is-invalid" id="accountForReset" v-model="state.resetForm.account">
+        <div class="col-sm-8">
+          <input type="text" :class="state.resetForm.accountClass" id="accountForReset" v-model="state.resetForm.account" @blur="handlerAccountForReset">
         </div>
       </div>
       <div class="form-group row">
         <label for="phoneForReset" class="col-sm-2 col-form-label">手机</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control is-invalid" id="phoneForReset" v-model="state.resetForm.phone">
+        <div class="col-sm-8">
+          <input type="text" :class="state.resetForm.phoneClass" id="phoneForReset" v-model="state.resetForm.phone" @blur="handlerPhoneForReset">
         </div>
+        <button class="col-sm-2 btn btn-primary" style="font-size: 6px;" @click="toGetCodeForReset">获取验证码</button>
       </div>
       <div class="form-group row">
         <label for="pwdForReset" class="col-sm-2 col-form-label">密码</label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control is-invalid" id="pwdForReset" v-model="state.resetForm.pwd">
+        <div class="col-sm-8">
+          <input type="password" :class="state.resetForm.pwdClass" id="pwdForReset" v-model="state.resetForm.pwd" @blur="handlerPwdForReset">
         </div>
       </div>
       <div class="form-group row">
         <label for="upwdForReset" class="col-sm-2 col-form-label">确认密码</label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control is-invalid" id="upwdForReset" v-model="state.resetForm.upwd">
+        <div class="col-sm-8">
+          <input type="password" :class="state.resetForm.upwdClass" id="upwdForReset" v-model="state.resetForm.upwd" @blur="handlerUpwdForReset">
         </div>
       </div>
       <div class="form-group row">
         <label for="codeForReset" class="col-sm-2 col-form-label">验证码</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control is-invalid" id="codeForReset" v-model="state.resetForm.code">
+        <div class="col-sm-8">
+          <input type="text" :class="state.resetForm.codeClass" id="codeForReset" v-model="state.resetForm.code" @blur="handlerCodeForReset">
         </div>
       </div>
       <div class="form-group row" style="margin-top: 50px;">
@@ -85,12 +87,96 @@ export default {
       findPwdStyle: 'border-bottom: 5px solid #003399;background-color: #F0FFFF',
       isFindPwd: true,
       findForm: {
-        account: '', phone: '', code: ''
+        account: '123', phone: '', code: '', accountClass: 'form-control', phoneClass: 'form-control', codeClass: 'form-control'
       },
       resetForm: {
-        account: '', phone: '', code: '', pwd: '', upwd: ''
+        account: '', phone: '', code: '', pwd: '', upwd: '', accountClass: 'form-control', phoneClass: 'form-control', codeClass: 'form-control', pwdClass: 'form-control', upwdClass: 'form-control'
       }
     })
+    // const findForm = reactive({
+    //   account: '', phone: '', code: '', accountClass: 'form-control', phoneClass: 'form-control', codeClass: 'form-control'
+    // })
+    // const resetForm = reactive({
+    //   account: '', phone: '', code: '', pwd: '', upwd: '', accountClass: 'form-control', phoneClass: 'form-control', codeClass: 'form-control', pwdClass: 'form-control', upwdClass: 'form-control'
+    // })
+    console.log(state.findForm.account)
+    const toGetCodeForFind = (state) => {
+      if (!state.findForm.phone) {
+        return true
+      } else {
+        alert('验证码：1234')
+      }
+    }
+
+    const toGetCodeForReset = (state) => {
+      if (!state.resetForm.phone) {
+        return true
+      } else {
+        alert('验证码：1234')
+      }
+    }
+    
+    const handlerAccountForFind = (state) => {
+      if (!state.findForm.account) {
+        state.findForm.accountClass = 'form-control is-invalid'
+      } else {
+        state.findForm.accountClass = 'form-control is-valid'
+      }
+    }
+
+    const handlerPhoneForFind = (state) => {
+      if (!state.findForm.phone) {
+        state.indForm.phoneClass = 'form-control is-invalid'
+      } else {
+        state.findForm.phoneClass = 'form-control is-valid'
+      }
+    }
+
+    const handlerCodeForFind = (state) => {
+      if (!state.findForm.code) {
+        state.findForm.codeClass = 'form-control is-invalid'
+      } else {
+        state.findForm.codeClass = 'form-control is-valid'
+      }
+    }
+
+    const handlerAccountForReset = (state) => {
+      if (!state.resetForm.account) {
+        state.resetForm.accountClass = 'form-control is-invalid'
+      } else {
+        state.resetForm.accountClass = 'form-control is-valid'
+      }
+    }
+
+    const handlerPhoneForReset = (state) => {
+      if (!state.resetForm.phone) {
+        state.resetForm.phoneClass = 'form-control is-invalid'
+      } else {
+        state.resetForm.phoneClass = 'form-control is-valid'
+      }
+    }
+
+    const handlerCodeForReset = (state) => {
+      if (!state.resetForm.code) {
+        state.resetForm.codeClass = 'form-control is-invalid'
+      } else {
+        state.resetForm.codeClass = 'form-control is-valid'
+      }
+    }
+    const handlerPwdForReset = (state) => {
+      if (!state.resetForm.pwd) {
+        state.resetForm.pwdClass = 'form-control is-invalid'
+      } else {
+        state.resetForm.pwdClass = 'form-control is-valid'
+      }
+    }
+    const handlerUpwdForReset = (state) => {
+      if (!state.resetForm.upwd) {
+        state.resetForm.upwdClass = 'form-control is-invalid'
+      } else {
+        state.resetForm.upwdClass = 'form-control is-valid'
+      }
+    }
     const showFindPwdForm = () => {
       state.findPwdStyle = 'border-bottom: 5px solid #003399;background-color: #F0FFFF'
       state.resetPwdStyle = ''
@@ -108,7 +194,7 @@ export default {
         router.push({
           path: '/login'
         })
-      }, 1000)
+      }, 500)
     }
     const toResetPassword = () => {
       console.log(state.resetForm)
@@ -116,11 +202,25 @@ export default {
       setTimeout(() => {
         router.push({
           path: '/login'
-        }, 1000)
+        }, 500)
       })
     }
     return {
-      state, showResetPwdForm, showFindPwdForm, toFindPassword, toResetPassword
+      state,
+      showResetPwdForm,
+      showFindPwdForm,
+      toFindPassword,
+      toResetPassword,
+      handlerAccountForFind,
+      handlerPhoneForFind,
+      handlerCodeForFind,
+      handlerAccountForReset,
+      handlerPhoneForReset,
+      handlerCodeForReset,
+      handlerPwdForReset,
+      handlerUpwdForReset,
+      toGetCodeForFind,
+      toGetCodeForReset
     }
   }
 }
