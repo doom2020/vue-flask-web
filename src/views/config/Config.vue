d41bc1f85857d2fd3f1e2b7237e02b1e17e1de93
yuanzhijian <v-yuanzhijian@xiaomi.com>
2021-01-22 18:29:55 +0800

    新增组件

---
+++ src/views/config/Config.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="config"></div>
+</template>
+
+<script>
+export default {
+  name: 'Config',
+  emits: [''],
+  props: {
+
+  },
+  setup(props, { emit }) {
+    return {}
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/message/Message.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="message"></div>
+</template>
+
+<script>
+export default {
+    name: 'Message',
+    emits: [],
+    props: {},
+    setup(props, { emit }) {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
